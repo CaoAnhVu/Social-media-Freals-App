0b72211c031ebf8e39373952bb3cf00e5e0e0dca
Andyvux <anhvuktnh@gmail.com>
2024-09-25 23:02:48 +0700

    update

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -36,10 +36,8 @@ const LeftSidebar = () => {
           <SignOutButton redirectUrl="/sign-in">
             <div className="flex cursor-pointer gap-4 p-4">
               <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
-
               <p className="text-light-2 max-lg:hidden">Logout</p>
             </div>
-            );
           </SignOutButton>
         </SignedIn>
       </div>
